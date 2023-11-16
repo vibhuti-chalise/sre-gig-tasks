839f2ddd913e229b9abf8cf3968162bb15ae2e34
Vibhuti Chalise <vchalise@vchalise.bne.csb>
2023-11-16 16:42:09 +1000

    update: array declaration, initialization, and append in main() wheras displayItem only prints array items using a for loop

--- task1/menu.go
+++ task1/menu.go
@@ -3,14 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var menu []string = []string{"Burrito", "Pasta"}
+	//Declare an array of 4 elements
+	var menu [4]string
 
-	displayItem(menu)
+	//Initialize the first elements of the array
+	menu = [4]string{"Burrito", "Pasta"}
+
+	//Add the second, third elements to an array
+	menu[3] = "Salad"
+	menu[2] = "Hamburger"
+
+	//Pass array elements by reference during a function call
+	displayItem(&menu)
 }
 
-func displayItem(food []string) {
-	food = append(food, "Hamburger")
-	food = append(food, "Salad")
+// Use a pointer variable to string array to point the address of each array items
+func displayItem(food *[4]string) {
 	for _, value := range food {
 		fmt.Println("\tFood:", value)
 	}
