8243a7fdee85ba6733139a90b96f8e973b96988d
Vibhuti Chalise <vchalise@vchalise.bne.csb>
2023-11-20 15:12:47 +1000

    task1 contents removed; and called rand.Intn() function directly from main()

--- task2/main.go
+++ task2/main.go
@@ -6,25 +6,21 @@ import (
 )
 
 func main() {
-	n := getNumber(100, 1)
-	fmt.Println("===================== Random number generated is ===================== ", n)
+	n := rand.Intn(100)
 	checkNumber(n)
-}
-
-func getNumber(max int, min int) int {
-	return rand.Intn(max - min)
 }
 
 func checkNumber(n int) {
 	if n > 50 {
 		if n%2 == 0 {
-			fmt.Println("\n\t\t\t\t It's closer to 100, and its even!\n")
+			fmt.Println("\t\t\t\t It's closer to 100, and its even!")
 		} else {
-			fmt.Println("\n\t\t\t\t It's closer to 100\n")
+			fmt.Println("\t\t\t\t It's closer to 100")
 		}
 	} else if n < 50 {
-		fmt.Println("\n\t\t\t\t It's closer to 0\n")
+		fmt.Println("\t\t\t\t It's closer to 0")
 	} else if n == 50 {
-		fmt.Println("\n\t\t\t\t It's 50 \n")
+		fmt.Println("\t\t\t\t It's 50")
 	}
+	fmt.Println("=========== Random number generated is ============== ", n)
 }
